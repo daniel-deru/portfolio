de4aa2ea0d928344de194a085ddc9023a088c149
daniel de ru <danielderu3@gmail.com>
2021-04-14 14:06:38 +0200

    took the build folder out of the client folder

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const __dirname = path.resolve(path.dirname(""))
 
 server.use(cors())
 server.use(express.json())
-server.use(express.static(path.join(__dirname, "client/build")))
+server.use(express.static(path.join(__dirname, "build")))
 
 server.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+    res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
 const PORT = process.env.PORT || 8000
